092e27eb11f8a65fde366f4da206e04e13ab8007
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-01 17:08:29 -0400

    Completed CRUD controllers.

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -19,13 +19,46 @@ const pizzaController = {
           res.status(404).json({ message: "No pizza found with this ID" });
           return;
         }
-
         res.json(data);
       })
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
       });
+  },
+
+  // Create a pizza
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then((data) => res.json(data))
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // Update a pizza
+  updatePizza({ params, body }, res) {
+    // new: true tells mongoose to return the new version
+    Pizza.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((data) => {
+        if (!data) {
+          res.status(404).json({ message: "No pizza found with this ID" });
+          return;
+        }
+        res.json(data);
+      })
+      .catch((err) => res.status(400).json(err));
+  },
+
+  // Delete a pizza
+  deletePizza({ params }, res) {
+    Pizza.findOneAndDelete({ _id: params.id })
+      .then((data) => {
+        if (!data) {
+          res.status(404).json({ message: "No pizza found with this ID" });
+          return;
+        }
+        res.json(data);
+      })
+      .catch((err) => res.status(400).json(err));
   },
 };
 
