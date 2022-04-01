fdb570571c91d5c1c843b8a09982e87a0af78c0f
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-01 18:19:35 -0400

    Added routes for the pizza controller.

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const {
+  getAllPizza,
+  getOnePizzaByID,
+  createPizza,
+  updatePizza,
+  deletePizza,
+} = require("../../controllers/pizza-controller");
+
+// Set up GET all and POST at /api/pizzas
+router.route("/").get(getAllPizza).post(createPizza);
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+router.route("/:id").get(getOnePizzaByID).put(updatePizza).delete(deletePizza);
+
+module.exports = router;
