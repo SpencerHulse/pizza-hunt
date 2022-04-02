f0e21b9dcea2dd86f8206ad73212e4d7cd629ab0
Spencer Hulse <hulse.spencer@gmail.com>
2022-04-02 12:52:26 -0400

    Connected the front-end to the back-end for Pizzas and Comments

--- models/Pizza.js
+++ models/Pizza.js
@@ -1,16 +1,23 @@
 const { Schema, model } = require("mongoose");
+const dateFormat = require("../utils/dateFormat");
 
 const PizzaSchema = new Schema(
   {
     pizzaName: { type: String },
     createdBy: { type: String },
-    createdAt: { type: Date, default: Date.now },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // A getter serves as a middleware, causing something to be done when retrieving data
+      get: (createdAtVal) => dateFormat(createdAtVal),
+    },
     size: { type: String, default: "Large" },
     toppings: [],
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
   },
   // ID is false because it is a virtual that Mongoose returns, so we don't need it
-  { toJSON: { virtuals: true }, id: false }
+  // Getters must be set to true to use the getters we create in the model above
+  { toJSON: { virtuals: true, getters: true }, id: false }
 );
 
 PizzaSchema.virtual("commentCount").get(function () {
